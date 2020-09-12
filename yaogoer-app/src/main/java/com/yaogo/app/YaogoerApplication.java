5523930038f9308a6b8f044b7f0ded6caa8d5fcf
irving <irvingzqy@gmail.com>
2020-09-12 00:27:24 -0400

    Try to ConfigurationPropertiesScan

--- yaogoer-app/src/main/java/com/yaogo/app/YaogoerApplication.java
+++ yaogoer-app/src/main/java/com/yaogo/app/YaogoerApplication.java
@@ -2,10 +2,12 @@ package com.yaogo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.yaogo")
+@ConfigurationPropertiesScan
 public class YaogoerApplication {
 
     public static void main(String[] args) {
