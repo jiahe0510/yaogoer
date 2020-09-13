98bc0f001bdb8470728e7ca6019e5b7cc575ce1c
IrvingZ <irvingzqy@gmail.com>
2020-09-12 23:14:15 -0400

    service getAll

--- yaogoer-services/src/main/java/com/yaogo/services/service/impl/UserServiceImpl.java
+++ yaogoer-services/src/main/java/com/yaogo/services/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.yaogo.services.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +21,9 @@ public class UserServiceImpl implements UserService {
         Optional<User> user = userRepo.findById(id);
 
         return user.orElseGet(User::new);
+    }
+
+    public List<User> getAll(){
+        return userRepo.findAll();
     }
 }
