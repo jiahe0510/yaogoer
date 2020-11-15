44783dc59aa69d98737ee177839ce76d1dc235b1
Jiahe Zhao <05jiahe10@gmail.com>
2020-11-15 02:01:27 -0500

    add getter setter to UserPrincipal

--- src/main/java/com/yaogo/config/security/UserPrincipal.java
+++ src/main/java/com/yaogo/config/security/UserPrincipal.java
@@ -64,4 +64,20 @@ public class UserPrincipal implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
